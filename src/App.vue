615746cc4a6cb935e3b0fdc0d4335bddb6ab431e
Bo Skakke <bo@skakke.dk>
2023-06-04 09:32:42 +0200

    loadmore

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const data = ref(null)
 const error = ref(null)
+const postsToShow = ref(5)
 
 fetch('https://api.scribblelive.com/v1/stream/3025174/posts?PageNumber=0&PageSize=99&NewestAtBottom=false&IncludeComments=false&token=9yyBmETN')
   .then((res) => res.json())
-  .then((json) => (data.value = json))
+  .then((json) => {
+    data.value = json
+    console.log(data.value)  
+    })
   .catch((err) => (error.value = err))
+
 
 </script>
 
@@ -15,11 +20,10 @@ fetch('https://api.scribblelive.com/v1/stream/3025174/posts?PageNumber=0&PageSiz
    <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
     <div v-else-if="data">
       
-      <div v-for="post in data.posts" :key="post.CreationDate">
-        <div v-html="post.Content"></div>
-        
+      <div v-for="(post, index) in postsToShow" :key="post.CreationDate">
+        <div v-html="data.posts[index].Content"></div>  
       </div>
-      {{ data.posts }}
+      <button @click="postsToShow += 5">Load more</button>
     </div>
     <div v-else>Loading...</div>
 </template>
